13b03c9f4b3900711025ba81ab98db33ddbd9516
Admin <anhquan300503@gmail.com>
2024-05-07 13:39:12 +0700

    cập nhật

--- Kiến thức/arduino/B8/B8.ino
+++ Kiến thức/arduino/B8/B8.ino
@@ -1,23 +1,28 @@
 // Xây dựng sketch với 3 led xanh đỏ vàng. yêu cầu: led xanh sáng/tắt với mỗi 3s, đền đỏ sáng khi có gọi ngắt 0, đèn vàng sáng khi gọi ngắt 1.
 const int pinGreen = 13, pinRed = 11, pinYellow = 9;
-const int btnRed = 5, btnYellow = 2;
+const int btnRed = 3, btnYellow = 2;
+const int timeDelay = 1500;
 
 void setup() {
+  Serial.begin(9600);
   pinMode(pinGreen, OUTPUT);
-  pinMode(pinRed, INPUT_PULLUP);
-  pinMode(pinYellow, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(pinRed), statusRed, FALLING);
-  attachInterrupt(digitalPinToInterrupt(pinYellow), statusYellow, FALLING);
+  pinMode(pinRed, OUTPUT);
+  pinMode(pinYellow, OUTPUT);
+  pinMode(btnRed, INPUT_PULLUP);
+  pinMode(btnYellow, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(btnRed), statusRed, FALLING);
+  attachInterrupt(digitalPinToInterrupt(btnYellow), statusYellow, FALLING);
 }
 
 void loop() {
   digitalWrite(pinGreen, HIGH);
-  delay(1500);
+  delay(timeDelay);
   digitalWrite(pinGreen, LOW);
-  delay(1500);
+  delay(timeDelay);
 }
 
 void statusRed() {
+  Serial.println("Da click !");
   digitalWrite(pinRed, !digitalRead(pinRed));
 }
 
