1249547f123e8cd2a1d698a59203d51dafcbc6b0
Admin <anhquan300503@gmail.com>
2024-09-14 10:48:21 +0700

    cập nhật

---
+++ source - project temporary/php/test_php/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://anhquan30052003.github.io/AnhQuanCSS/style-main.css">
+
+  <title>Form PHP</title>
+</head>
+
+<body>
+  <form
+    action="show.php"
+    method="post"
+    class="m-10"
+    >
+    <label for="">Nhập tên bạn: </label>
+    <input type="text" name="nameCard" id="" class="p-5"> 
+    <input type="submit"
+      class="p-5"
+      value="Add"
+      >
+  </form>
+</body>
+</html>
