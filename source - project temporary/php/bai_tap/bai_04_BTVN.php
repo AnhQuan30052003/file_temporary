f6996b7585d90a2aff3c1169fe238c0cb5f58000
Admin <anhquan300503@gmail.com>
2024-09-23 12:32:00 +0700

    cập nhật

--- source - project temporary/php/bai_tap/bai_04_BTVN.php
+++ source - project temporary/php/bai_tap/bai_04_BTVN.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Bảng Cửu Chương</title>
 
   <style>
     .box {
@@ -17,32 +17,29 @@
 
 <body>
   <table>
-    <tbody>
-      <tr>
-        <?php
-          for ($i = 1; $i <= 5; $i++) {
-            echo "<td class='box'>";
-            for ($j = 1; $j <= 10; $j++) {
-              echo "$i x $j = " . $i * $j . "<br>";
-            }
-            echo "</td>";
+    <tr>
+      <?php
+        for ($i = 1; $i <= 5; $i++) {
+          echo "<td class='box'>";
+          for ($j = 1; $j <= 10; $j++) {
+            echo "$i x $j = " . $i * $j . "<br>";
           }
-        ?> 
-      </tr>
+          echo "</td>";
+        }
+      ?> 
+    </tr>
           
-      <tr>
-        <?php
-          for ($i = 6; $i <= 9; $i++) {
-            echo "<td class='box'>";
-            for ($j = 1; $j <= 10; $j++) {
-              echo "$i x $j = " . $i * $j . "<br>";
-            }
-            echo "</td>";
+    <tr>
+      <?php
+        for ($i = 6; $i <= 9; $i++) {
+          echo "<td class='box'>";
+          for ($j = 1; $j <= 10; $j++) {
+            echo "$i x $j = " . $i * $j . "<br>";
           }
-        ?> 
-      </tr>
-
-    </tbody>
+          echo "</td>";
+        }
+      ?> 
+    </tr>
   </table>
 </body>
 </html>
